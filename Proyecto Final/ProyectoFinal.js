1d301305380e5d16fca4206c8ee3f300c8ba8bbd
FacundoReyesZ <rfzamboni7@gmail.com>
2022-08-10 17:55:20 -0300

    fix 3

--- Proyecto Final/ProyectoFinal.js
+++ Proyecto Final/ProyectoFinal.js
@@ -1,13 +1,18 @@
 let snatch = []
 let cleanAndJerk = []
 let clean =[]
+let usuarioNuevo=true
+
+do{
+
 let nombreAtleta = prompt(`Porfavor ingrese su nombre`)
 
 let herramientaRm =confirm(`Hola ${nombreAtleta} queres utilizar la calculadora de repeticiones maximas?`)
+    
 if(herramientaRm===true){
-let rm = prompt(`Hola ${nombreAtleta} , porfavor indicame tu repeticion maxima para darte los porcentajes en kilogramos.`)
-porcentajes(rm)
-}
+    let rm = prompt(`Hola ${nombreAtleta} , porfavor indicame tu repeticion maxima para darte los porcentajes en kilogramos.`)
+    porcentajes(rm)
+    }
 
 let ingresarMarca= confirm(`Tienes alguna nueva marca de repeticion maxima que quieras agregar a la tabla?`)
 
@@ -17,16 +22,22 @@ if(ingresarMarca===true){
     if(movimiento=="snatch"){
         snatch.push({nombre:nombreAtleta,marca:nuevaMarca});
         console.table(snatch);
-    }
+}
     if(movimiento=="cleanAndJerk"){
         cleanAndJerk.push({nombre:nombreAtleta,marca:nuevaMarca});
         console.table(cleanAndJerk);
-    }
+}
     if(movimiento=="clean"){
         clean.push({nombre:nombreAtleta,marca:nuevaMarca});
         console.table(clean);
-    }
 }
+}
+    
+
+    usuarioNuevo=confirm(`¿Desea agregar otra marca?`)
+        
+}while(usuarioNuevo===true)
+
 
 
 
--- Proyecto Final/funciones.js
+++ Proyecto Final/funciones.js
@@ -4,6 +4,32 @@ function porcentajes(rm){
     }
 }
 
+function calculadora(){
+    if(herramientaRm===true){
+        let rm = prompt(`Hola ${nombreAtleta} , porfavor indicame tu repeticion maxima para darte los porcentajes en kilogramos.`)
+        porcentajes(rm)
+        }
+    }
+
+function registroDeRm(){
+    if(ingresarMarca===true){
+        let movimiento=prompt(`¿Cual es el movimiento?`)
+        let nuevaMarca=prompt(`Porfavor indica el peso en kilogramos de la nueva marca`)
+        if(movimiento=="snatch"){
+            snatch.push({nombre:nombreAtleta,marca:nuevaMarca});
+            console.table(snatch);
+    }
+        if(movimiento=="cleanAndJerk"){
+            cleanAndJerk.push({nombre:nombreAtleta,marca:nuevaMarca});
+            console.table(cleanAndJerk);
+    }
+        if(movimiento=="clean"){
+            clean.push({nombre:nombreAtleta,marca:nuevaMarca});
+            console.table(clean);
+    }
+    }
+    }
+
 class Atleta{
     constructor(nombre,rmSnatch,rmClean,rmCleanAndJerk){
         this.nombre=nombre;
