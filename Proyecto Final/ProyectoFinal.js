b3a874185e5289250f1c28170036b2ff49fa74d2
FacundoReyesZ <rfzamboni7@gmail.com>
2022-08-15 19:56:16 -0300

    upgrade

--- Proyecto Final/ProyectoFinal.js
+++ Proyecto Final/ProyectoFinal.js
@@ -3,12 +3,22 @@ let snatch = []
 let cleanAndJerk = []
 let clean =[]
 let usuarioNuevo=true
+let bienvenida = document.getElementById("bienvenida")
+
 
 let nombreAtleta = prompt(`Porfavor ingrese su nombre`)
 let primerRegistro=confirm(`Hola ${nombreAtleta}, si es tu primer registro , necesito que cargues tus repeticiones maximas de Snatch , C&J y Squat Clean `)
 if(primerRegistro===true){
     atletas.push(new Atleta(nombreAtleta,prompt(`coloque su rm de snatch`),prompt(`coloque su rm de clean`),prompt(`coloque su rm de clean and jerk`)))
 }
+
+
+function cambiarEncabezado(){
+    bienvenida.innerText = `Hola ${nombreAtleta} ,Bienvenido a tu app para atletas`
+}
+cambiarEncabezado()
+
+
 
 do{
 
