c4134b70ba0bf0df57a1b3b07f0fb75433efa045
FacundoReyesZ <rfzamboni7@gmail.com>
2022-08-16 09:33:42 -0300

    upgrade

--- Proyecto Final/ProyectoFinal.js
+++ Proyecto Final/ProyectoFinal.js
@@ -3,8 +3,9 @@ let snatch = []
 let cleanAndJerk = []
 let clean =[]
 let usuarioNuevo=true
-let bienvenida = document.getElementById("bienvenida")
-
+let bienvenida = document.getElementById("bienvenida");
+let calculadora = document.getElementById("calculadora")
+let repMax=document.getElementById("repMax")
 
 let nombreAtleta = prompt(`Porfavor ingrese su nombre`)
 let primerRegistro=confirm(`Hola ${nombreAtleta}, si es tu primer registro , necesito que cargues tus repeticiones maximas de Snatch , C&J y Squat Clean `)
@@ -12,45 +13,12 @@ if(primerRegistro===true){
     atletas.push(new Atleta(nombreAtleta,prompt(`coloque su rm de snatch`),prompt(`coloque su rm de clean`),prompt(`coloque su rm de clean and jerk`)))
 }
 
+bienvenida.innerText = `Hola ${nombreAtleta} ,Bienvenido a tu app para atletas`
 
+calculadora.addEventListener("click",fcalculadora)    
 
-    bienvenida.innerText = `Hola ${nombreAtleta} ,Bienvenido a tu app para atletas`
-
-
-
-
-do{
-
-let herramientaRm =confirm(`Hola ${nombreAtleta} queres utilizar la calculadora de repeticiones maximas?`)
+repMax.addEventListener("click",ingresarRm)
     
-if(herramientaRm===true){
-    let rm = prompt(`Hola ${nombreAtleta} , porfavor indicame tu repeticion maxima para darte los porcentajes en kilogramos.`)
-    porcentajes(rm)
-    }
-
-let ingresarMarca= confirm(`Tienes alguna nueva marca de repeticion maxima que quieras agregar a la tabla?`)
-
-if(ingresarMarca===true){
-    let movimiento=prompt(`¿Cual es el movimiento?`)
-    let nuevaMarca=prompt(`Porfavor indica el peso en kilogramos de la nueva marca`)
-    if(movimiento=="snatch"){
-        snatch.push({nombre:nombreAtleta,marca:nuevaMarca});
-        console.table(snatch);
-}
-    if(movimiento=="cleanAndJerk"){
-        cleanAndJerk.push({nombre:nombreAtleta,marca:nuevaMarca});
-        console.table(cleanAndJerk);
-}
-    if(movimiento=="clean"){
-        clean.push({nombre:nombreAtleta,marca:nuevaMarca});
-        console.table(clean);
-}
-}
-    
-
-    usuarioNuevo=confirm(`¿Desea agregar otra marca?`)
-        
-}while(usuarioNuevo===true)
 
 
 
--- Proyecto Final/funciones.js
+++ Proyecto Final/funciones.js
@@ -1,16 +1,45 @@
 
 function porcentajes(rm){
-    for(let i=1;i<10;i++){
-        console.log(`Este es tu ${i*10}%:`,Math.round( rm * (0.10*i)))
+    for(let i=4;i<10;i++){
+        const lista=document.getElementById("lista")
+        const li=document.createElement("li")
+              li.innerText=`Este es tu ${i*10}%: ${Math.round( rm * (0.10*i))}`
+              lista.append(li)
+        /* console.log(`Este es tu ${i*10}%:`,Math.round( rm * (0.10*i))) */
     }
 }
 
-function calculadora(){
-    if(herramientaRm===true){
-        let rm = prompt(`Hola ${nombreAtleta} , porfavor indicame tu repeticion maxima para darte los porcentajes en kilogramos.`)
-        porcentajes(rm)
-        }
-    }
+function fcalculadora(){
+    let rm = prompt(`Hola ${nombreAtleta} , porfavor indicame tu repeticion maxima para darte los porcentajes en kilogramos.`)
+    porcentajes(rm)
+}
+
+function ingresarRm(){
+    do{
+    let ingresarMarca= confirm(`Tienes alguna nueva marca de repeticion maxima que quieras agregar a la tabla?`)
+
+if(ingresarMarca===true){
+let movimiento=prompt(`¿Cual es el movimiento?`)
+let nuevaMarca=prompt(`Porfavor indica el peso en kilogramos de la nueva marca`)
+if(movimiento=="snatch"){
+    snatch.push({nombre:nombreAtleta,marca:nuevaMarca});
+    console.table(snatch);
+}
+if(movimiento=="cleanAndJerk"){
+    cleanAndJerk.push({nombre:nombreAtleta,marca:nuevaMarca});
+    console.table(cleanAndJerk);
+}
+if(movimiento=="clean"){
+    clean.push({nombre:nombreAtleta,marca:nuevaMarca});
+    console.table(clean);
+}
+}
+
+
+usuarioNuevo=confirm(`¿Desea agregar otra marca?`)
+    
+}while(usuarioNuevo===true)
+}
 
 function registroDeRm(){
     if(ingresarMarca===true){
